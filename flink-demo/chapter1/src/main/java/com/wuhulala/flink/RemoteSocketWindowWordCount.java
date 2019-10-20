f28cbbd915d6f7b5ac39c250661a2f7ba8bb5e25
wuhulala <370031044@qq.com>
2019-10-20 18:01:33 +0800

    :tada: 一些小例子

---
+++ flink-demo/chapter1/src/main/java/com/wuhulala/flink/RemoteSocketWindowWordCount.java
@@ -0,0 +1,68 @@
+package com.wuhulala.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.util.Collector;
+
+public class RemoteSocketWindowWordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration config = new Configuration();
+        config.setString("jobmanager.rpc.address", "jobmanager");
+        config.setString("jobmanager.rpc.port", "6123");
+
+        // get the execution environment
+        final ExecutionEnvironment env = ExecutionEnvironment.createRemoteEnvironment("localhost", 8081, "D:\\01code\\personal\\flink\\flink-demo\\chapter1\\target\\chapter1-1.0-SNAPSHOT.jar");
+
+        // get input data by connecting to the socket
+        DataSource<String> text = env.readTextFile("D:\\01code\\personal\\flink\\flink-demo\\chapter1\\src\\main\\resources\\test\\hello.txt");
+
+        env.setParallelism(3);
+        // parse the data, group it, window it, and aggregate the counts
+        text
+                .flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    @Override
+                    public void flatMap(String value, Collector<WordWithCount> out) {
+                        for (String word : value.split(" ")) {
+                            System.out.println(word);
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                })
+                .groupBy("word")
+                .reduce(new ReduceFunction<WordWithCount>() {
+                    @Override
+                    public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+                        return new WordWithCount(a.word, a.count + b.count);
+                    }
+                }).collect()
+                .forEach(wordWithCount -> {
+                    System.out.println("show:::" + wordWithCount);
+                });
+        //env.execute("file Window WordCount");
+    }
+
+    // Data type for words with count
+    public static class WordWithCount {
+
+        public String word;
+        public long count;
+
+        public WordWithCount() {
+        }
+
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return word + " : " + count;
+        }
+    }
+}

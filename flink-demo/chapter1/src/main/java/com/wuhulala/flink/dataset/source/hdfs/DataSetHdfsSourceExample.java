a01519f9b47b0bfb388343ba408cfb019c58206e
wuhulala <370031044@qq.com>
2019-10-21 21:18:30 +0800

    :bento: 添加hdfs依赖

---
+++ flink-demo/chapter1/src/main/java/com/wuhulala/flink/dataset/source/hdfs/DataSetHdfsSourceExample.java
@@ -0,0 +1,18 @@
+package com.wuhulala.flink.dataset.source.hdfs;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+
+/**
+ * @author wuhulala<br>
+ * @date 2019/10/20<br>
+ * @since v1.0<br>
+ */
+public class DataSetHdfsSourceExample {
+
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> text = env.readTextFile("hdfs://flinkhadoop:29000/wordcount/input/README.txt");
+        text.print();
+    }
+}
